aec2df856c355514d1c7312c9bb6efa7a0e1b785
Lu Li <luli2@u91z03s03.42kl.edu.my>
2026-08-05 20:57:35 +0800

    py2

--- PY/py01/ex0/ft_garden_intro.py
+++ PY/py01/ex0/ft_garden_intro.py
@@ -5,7 +5,7 @@ def ft_garden_intro() -> None:
     name: str = "Rose"
     height: int = 25
     age: int = 30
-    
+
     print("=== Welcome to My Garden ===")
     print(f"Plant: {name}")
     print(f"Height: {height}cm")
---
+++ PY/py02/ex0/ft_first_exception.py
@@ -0,0 +1,17 @@
+def input_temperature(temp_str: str) -> int:
+	return int(temp_str)
+
+def test_temperature(temp_str: str) -> None:
+    print(f"Input data is '{temp_str}'")
+    try:
+        temp = input_temperature(temp_str)
+        print(f"Temperature is now {temp}")
+    except ValueError as e:
+        print(f"Caught input_temperature error: {e}")
+
+if __name__ == "__main__":
+    print("=== Garden Temperature ===")
+    print()
+    test_temperature("25")
+    print()
+    test_temperature("abc")
